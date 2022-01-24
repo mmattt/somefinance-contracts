98ea38fc8dd90158277cd26c75cdb3f84fb8b63f
Yanhao <tyanhao@gmail.com>
2022-01-24 16:27:13 +0800

    Deploy TShare

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,11 @@
 var DummyToken = artifacts.require("DummyToken");
 var Tomb = artifacts.require("Tomb");
 var Treasury = artifacts.require("Treasury");
+var TShare = artifacts.require("TShare");
 var Masonry = artifacts.require("Masonry")
 
 module.exports = function(deployer) {
   deployer.deploy(DummyToken);
   deployer.deploy(Masonry);
+  deployer.deploy(TShare, 1643012641, "0xeDFDA8DB4a16e1065E8324fA60f1c6eeBccd6783", "0x56722dd61D8616071907005b483529d1eb62F46b");
 };
