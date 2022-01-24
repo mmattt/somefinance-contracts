22455252137fcbb5bd6939d0ccc218e0ba06875d
Yanhao <tyanhao@gmail.com>
2022-01-24 16:17:42 +0800

    Add Masonry Contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,9 @@
 var DummyToken = artifacts.require("DummyToken");
 var Tomb = artifacts.require("Tomb");
 var Treasury = artifacts.require("Treasury");
+var Masonry = artifacts.require("Masonry")
 
 module.exports = function(deployer) {
   deployer.deploy(DummyToken);
-  deployer.deploy(Tomb);
+  deployer.deploy(Masonry);
 };
