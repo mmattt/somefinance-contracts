0065dd68509151dab15337abfcb3206b540ca4ce
Yanhao <tyanhao@gmail.com>
2022-01-24 16:49:41 +0800

    Add TShare

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -14,5 +14,6 @@ var Treasury = artifacts.require("Treasury");  // TODO
 module.exports = function(deployer) {
   deployer.deploy(DummyToken);
   deployer.deploy(Masonry);
-  deployer.deploy(TShare, 1643012641, "0xeDFDA8DB4a16e1065E8324fA60f1c6eeBccd6783", "0x56722dd61D8616071907005b483529d1eb62F46b");
+  deployer.deploy(Tomb, 1000 /* 10% tax rate */, "0x446cACaBFF2b1A98a195f6532aAD9184619d41da");
+  deployer.deploy(TShare, 1643012641 /* start time */ , "0xeDFDA8DB4a16e1065E8324fA60f1c6eeBccd6783", "0x56722dd61D8616071907005b483529d1eb62F46b");
 };
